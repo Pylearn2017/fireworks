2a544d5c35318bd1fa26f45a4b095b26c96d90e0
avlitvin73 <avlitvin73@gmail.com>
2022-05-07 11:01:11 +0300

    fix: crate window

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 def crete_heroes(x,y):
     angle = 0
     heroes_list = []
-    num_balls = random.randint(9, 19)
+    num_balls = random.randint(7, 12)
     for i in range(num_balls):
         hero = turtle.Turtle()
         hero.shape('circle')
-        hero.shapesize(0.3)
+        hero.shapesize(random.randint(1, 3) / 10)
         hero.setposition(x,y)
         random_color = (random.random(), 
                     random.random(),
@@ -17,17 +17,27 @@ def crete_heroes(x,y):
         hero.color(random_color)
         hero.penup()
         hero.speed(0)
-        angle += 360/num_balls
+        angle += 360/num_balls + random.randint(-10, 10)
         hero.setheading(angle)
         hero.line = 100
         heroes_list.append(hero)
     return heroes_list
 
+def create_window():
+    window = turtle.Screen()
+    window.bgcolor('#000000')
+    window.tracer(0)
+    window.onclick(fire)
+
 def fire(x,y):
+    if len(window.turtles()) > 30:
+        window.clear()
+        create_window()
+    print(len(window.turtles()))
     heroes_list = crete_heroes(x,y)
     for i in range(100):
         for hero in window.turtles():
-            step = random.randint(0, 2)
+            step = random.randint(0, 1)
             hero.forward(step)
             hero.line -= 1
             if not hero.line:
