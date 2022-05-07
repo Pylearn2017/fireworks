3612e09f7f3d822de1be10e9f4c8f7534308ffc6
avlitvin73 <avlitvin73@gmail.com>
2022-05-07 10:31:57 +0300

    feat: random colors

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def fire(x,y):
     for hero in heroes_list:
         hero.setheading(angle)
         angle += 40 + random.randint(-10, 10)
-    size = random.randint(30, 50)
+    size = random.randint(30, 70)
     for i in range(size):
         for hero in heroes_list:
-            step = random.randint(1, 10)
+            step = random.randint(0, 2)
             hero.forward(step)
 
 window = turtle.Screen()
@@ -22,8 +22,12 @@ num_balls = random.randint(9, 19)
 for i in range(num_balls):
     hero = turtle.Turtle()
     hero.shape('circle')
-    hero.shapesize(0.5)
-    hero.color('#ffffff')
+    hero.shapesize(0.3)
+    random_color = (random.random(), 
+                random.random(),
+                random.random()
+                )
+    hero.color(random_color)
     hero.penup()
     hero.speed(0)
     heroes_list.append(hero)
