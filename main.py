752f543a86fab5faf81fd8963ba9f3018b46dc0d
avlitvin73 <avlitvin73@gmail.com>
2022-05-07 10:22:55 +0300

    feat: slow fire

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import turtle
 
 def fire(x,y):
-    hero.setposition(x,y)
+    angle = 0
+    for hero in heroes_list:
+        hero.setposition(x,y)
+    for hero in heroes_list:
+        hero.setheading(angle)
+        angle += 40
+    for i in range(50):
+        for hero in heroes_list:
+            hero.forward(10)
 
 window = turtle.Screen()
 window.bgcolor('#000000')
 
-hero = turtle.Turtle()
-hero.shape('circle')
-hero.color('#ffffff')
+heroes_list = []
+for i in range(9):
+    hero = turtle.Turtle()
+    hero.shape('circle')
+    hero.color('#ffffff')
+    hero.penup()
+    heroes_list.append(hero)
 
 
 window.onclick(fire)
